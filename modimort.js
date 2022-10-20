a53120f3de8bd4f466d4ce39acc0bac52eb25d05
tutgirl <104901786+tutgirl@users.noreply.github.com>
2022-10-20 08:45:38 -0500

    Add files via upload

--- modimort.js
+++ modimort.js
@@ -1,5 +1,66 @@
 // const devtools = require('../node_modules/devtools-detect/index.js');
-import devtools from '../node_modules/index.js';
+
+
+/*!
+devtools-detect
+https://github.com/sindresorhus/devtools-detect
+By Sindre Sorhus
+MIT License
+*/
+
+const devtools = {
+	isOpen: false,
+	orientation: undefined,
+};
+
+const threshold = 160;
+
+const emitEvent = (isOpen, orientation) => {
+	globalThis.dispatchEvent(new globalThis.CustomEvent('devtoolschange', {
+		detail: {
+			isOpen,
+			orientation,
+		},
+	}));
+};
+
+const main = ({emitEvents = true} = {}) => {
+	const widthThreshold = globalThis.outerWidth - globalThis.innerWidth > threshold;
+	const heightThreshold = globalThis.outerHeight - globalThis.innerHeight > threshold;
+	const orientation = widthThreshold ? 'vertical' : 'horizontal';
+
+	if (
+		!(heightThreshold && widthThreshold)
+		&& ((globalThis.Firebug && globalThis.Firebug.chrome && globalThis.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)
+	) {
+		if ((!devtools.isOpen || devtools.orientation !== orientation) && emitEvents) {
+			emitEvent(true, orientation);
+		}
+
+		devtools.isOpen = true;
+		devtools.orientation = orientation;
+	} else {
+		if (devtools.isOpen && emitEvents) {
+			emitEvent(false, undefined);
+		}
+
+		devtools.isOpen = false;
+		devtools.orientation = undefined;
+	}
+};
+
+main({emitEvents: false});
+setInterval(main, 500);
+
+export default devtools;
+
+
+
+
+
+
+
+// import devtools from '../node_modules/devtools-detect/index.js';
 
 window.addEventListener('load', function(){
     
@@ -12,18 +73,77 @@ console.log('DevTools orientation:', devtools.orientation);
 // Get notified when it's opened/closed or orientation changes
 window.addEventListener('devtoolschange', event => {
     alert('You are not Authorized to view source Code, Goodbye')
-    window.location.replace('https://tellernode.000webhostapp.com/index.html')
+    window.location.replace('test.html')
   console.log('Is DevTools open:', event.detail.isOpen);
   console.log('DevTools orientation:', event.detail.orientation);
 });
 
 window.addEventListener('devtools.isOpen', event => {
     alert('You are not Authorized to view source Code, Goodbye')
-    window.location.replace('https://tellernode.000webhostapp.com/index.html')
+    window.location.replace('test.html')
 });
 
 
 })
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', function(){
+    
+   // Check if it's open
+   console.log('Is DevTools open:', devtools.isOpen);
+   
+   // Check it's orientation, `undefined` if not open
+   console.log('DevTools orientation:', devtools.orientation);
+   
+   // Get notified when it's opened/closed or orientation changes
+   window.addEventListener('devtoolschange', event => {
+       alert('You are not Authorized to view source Code, Goodbye')
+       window.location.replace('test.html')
+     console.log('Is DevTools open:', event.detail.isOpen);
+     console.log('DevTools orientation:', event.detail.orientation);
+   });
+   
+   // window.addEventListener('devtools.isOpen', event => {
+   
+   // });
+   
+   if(devtools.isOpen){
+    alert('You are not Authorized to view source Code, Goodbye')
+       window.location.replace('test.html')
+   }
+  
+   
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // document.getElementById("ema").addEventListener('onchange', function(){
